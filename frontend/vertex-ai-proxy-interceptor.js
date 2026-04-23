1dfe750cb1fc780abd0866e8a8a6dd55fdff3976
Dinerpordin <shimul131@hotmail.com>
2026-04-23 01:45:50 +0100

    fine tune 2

--- frontend/vertex-ai-proxy-interceptor.js
+++ frontend/vertex-ai-proxy-interceptor.js
@@ -113,7 +113,7 @@
           headers: {
             'Content-Type': 'application/json',
             // Add a random header to identify these proxied requests on the Node.js backend.
-            'X-App-Proxy': '-HANbIXkyaloZZvnLHftKToXvFRcHMzq',
+            'X-App-Proxy': 'L4Wm9eUA_AANhzITLpg4M6GED_Hed1AG',
           },
           body: JSON.stringify(requestDetails),
         };
